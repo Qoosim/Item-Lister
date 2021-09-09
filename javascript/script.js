5f6ced6c0faa97bd398bf69a8c68ac4365f4bf78
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-09-09 22:15:45 +0100

    Complete UI to add items to the page

--- javascript/script.js
+++ javascript/script.js
@@ -2,14 +2,18 @@ const body = document.querySelector('body');
 console.log(body);
 const script = document.querySelector('script');
 
-const headerDiv = document.createElement('div');
+// Overall container
+const overallContainer = document.createElement('div');
+overallContainer.className = 'overall';
+
+// Header Section
 
 const header = document.createElement('div');
 header.className = 'bg-success text-white p-4 mb-3';
 header.id = 'main-header';
 
-const container = document.createElement('div');
-container.className = 'container';
+const headerContainer = document.createElement('div');
+headerContainer.className = 'container';
 
 const row = document.createElement('div');
 row.className = 'row';
@@ -34,9 +38,58 @@ input.setAttribute('placeholder', 'Search Items...');
 secondColumn.appendChild(input);
 row.appendChild(secondColumn);
 
-container.appendChild(row)
+headerContainer.appendChild(row)
 
-header.appendChild(container);
+header.appendChild(headerContainer);
+
+// Body content
+
+const bodyContainer = document.createElement('div');
+bodyContainer.className = 'container';
+
+const card = document.createElement('div');
+card.className = 'card card-body';
+card.id = 'main';
+
+const h2 = document.createElement('h2');
+h2.className = 'title';
+h2.textContent = 'Add Items';
+
+const form = document.createElement('form');
+form.id = 'addForm';
+form.className = 'form-inline mb-3';
+
+const formInput1 = document.createElement('input');
+formInput1.setAttribute('type', 'text');
+formInput1.id = 'item';
+formInput1.className = 'form-control mr-2'
+
+const formInput2 = document.createElement('input');
+formInput2.setAttribute('type', 'submit');
+formInput2.className = 'btn btn-dark';
+formInput2.setAttribute('value', 'Submit');
+
+const new_h2 = document.createElement('h2');
+new_h2.className = 'title';
+new_h2.textContent = 'Items'
+
+const ul = document.createElement('ul');
+ul.id = 'items';
+ul.className = 'list-group';
 
 
-body.insertBefore(header, script);
+form.appendChild(formInput1);
+form.appendChild(formInput2);
+card.appendChild(h2);
+card.appendChild(form);
+card.appendChild(new_h2);
+card.appendChild(ul);
+
+bodyContainer.appendChild(card);
+
+overallContainer.appendChild(header);
+overallContainer.appendChild(bodyContainer);
+body.insertBefore(overallContainer, script);
+
+
+
